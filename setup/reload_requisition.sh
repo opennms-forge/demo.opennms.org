8727da55921996b226abf504315478b8d158efe7
root <root@demo01.internal.opennms.com>
2018-08-13 17:38:15 -0400

    equalled variable names

--- setup/reload_requisition.sh
+++ setup/reload_requisition.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 programname=$0
-ONMS_ACCOUNT=admin
-ONMS_PASSWORD=admin
-ONMS_IP=127.0.0.1
-
+OPENNMS_HOST=127.0.0.1
+OPENNMS_USER=admin
+OPENNMS_PASS=admin
+OPENNMS_PORT=8980
+PRIS_HOST=pris
+PRIS_PORT=8000
 
 function usage {
     echo "usage: $programname [requisition-name]"
@@ -15,29 +17,29 @@ function usage {
 if [ "$#" -ne 1 ]; then
     echo "Choose a requisition to reload"
     echo "------------------------------"
-    curl -s -u $ONMS_ACCOUNT:$ONMS_PASSWORD http://$ONMS_IP:8980/opennms/rest/requisitions | xmllint --format - | grep foreign-source | awk '{print $3}' | cut -d"\"" -f2
+    curl -s -u $OPENNMS_USER:$OPENNMS_PASS http://$OPENNMS_HOST:$OPENNMS_PORT/opennms/rest/requisitions | xmllint --format - | grep foreign-source | awk '{print $3}' | cut -d"\"" -f2
     echo "------------------------------"
     usage
 fi
 
-for i in $(curl -s -u $ONMS_ACCOUNT:$ONMS_PASSWORD http://$ONMS_IP:8980/opennms/rest/requisitions | xmllint --format - | grep foreign-source | awk '{print $3}' | cut -d"\"" -f2)
+for i in $(curl -s -u $OPENNMS_USER:$OPENNMS_PASS http://$OPENNMS_HOST:$OPENNMS_PORT/opennms/rest/requisitions | xmllint --format - | grep foreign-source | awk '{print $3}' | cut -d"\"" -f2)
 do
  if [[ $i = ${1} ]]; then
   if 
     curl -s \
-         -u $ONMS_ACCOUNT:$ONMS_PASSWORD \
+         -u $OPENNMS_USER:$OPENNMS_PASS \
          -X POST \
          -d "<event> \
               <uei>uei.opennms.org/internal/importer/reloadImport</uei> \
               <parms> \
                <parm> \
                 <parmName>url</parmName> \
-                <value>http://pris:8000/requisitions/${1}</value> \
+                <value>http://$PRIS_HOST:$PRIS_PORT/requisitions/${1}</value> \
                </parm> \
               </parms> \
              </event>" \
          -H "Content-Type: application/xml" \
-         http://$ONMS_IP:8980/opennms/rest/events; then
+         http://$OPENNMS_HOST:$OPENNMS_PORT/opennms/rest/events; then
 
    echo "Requisition ${1} successfully reloaded"
    exit 1
