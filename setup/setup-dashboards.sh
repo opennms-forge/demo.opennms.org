02b1b57c8016be798ff1744bae4262ac54bd6389
Marcel Fuhrmann <mfuhrmann@mailbox.org>
2019-02-21 21:14:34 +0100

    added web response time board

--- setup/setup-dashboards.sh
+++ setup/setup-dashboards.sh
@@ -4,7 +4,7 @@ GF_USER="admin"
 GF_PASS="$(grep ADMIN_PASSWORD ../.grafana.env | awk -F "=" '{ print $2 }')"
 GF_URL=http://localhost:3000
 GRAFANA_DATASOURCE="OpenNMS Horizon PM"
-ds=(4033 4036 5053 9488 4046 6073 6070 5053);
+ds=(4033 4036 5053 9488 4046 6073 6070 5053 9775);
 for d in "${ds[@]}"; do
   echo -n "Processing $d: "
   j=$(curl -s -k -u "${GF_USER}:${GF_PASS}" ${GF_URL}/api/gnet/dashboards/"$d" | jq .json)
