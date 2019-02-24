20aae40ce6c12f68c15c9e49d7b96bd52fe1a8ce
Marcel Fuhrmann <mfuhrmann@mailbox.org>
2019-02-24 22:28:58 +0100

    bash syntax fix #3

--- setup/http_outages.sh
+++ setup/http_outages.sh
@@ -5,16 +5,16 @@
 set -u
 set -e
 
-nginxcount=$(docker ps | grep demo_nginx| wc -l)
-if (( $nginxcount < 3 ))
+nginxcount=$(docker ps | grep -c demo_nginx)
+if ( $nginxcount < 3 )
 then
   INSTALL_PATH=$(cat /etc/docker/.onms_install_path)
-  cd $INSTALL_PATH
+  cd "$INSTALL_PATH"
   docker-compose up -d
 else
  no=$(shuf -i 1-3 -n 1)
  echo "Shutting down demo_nginx$no..."
- docker stop demo_nginx$no 1>/dev/null
+ docker stop demo_nginx"$no" 1>/dev/null
  echo "Done!"
 fi
 
