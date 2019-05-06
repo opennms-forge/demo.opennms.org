2d7e6c9078a62c3b02da0180c3a6d6221eb1c9ca
root <root@rmvm056.ad1.proemion.com>
2019-05-06 22:23:13 +0000

    Bash syntax fixes for node_outages.sh

--- setup/node_outages.sh
+++ setup/node_outages.sh
@@ -12,9 +12,9 @@ then
   cd "$INSTALL_PATH" || exit 2
   cd .. &&  docker-compose up -d
 else
-  array=( $(docker ps --format '{{.Names}}' --filter "name=dc.*" --filter "name=elastic*"))
+  mapfile -t array < <(docker ps --format '{{.Names}}' --filter "name=dc.*" --filter "name=elastic*")
   no=$(shuf -i 1-5 -n 1)
-  echo "Shutting down node "${array[$no]}"..."
+  echo "Shutting down node ${array[$no]}..."
   docker stop "${array[$no]}" 1>/dev/null
   echo "Done!"
 fi
